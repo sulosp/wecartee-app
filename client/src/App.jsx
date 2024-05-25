e6a58eeb06dad22687712b32ec36425866f36e75
Sulochana P. <sulochanasp@hotmail.com>
2024-05-25 10:15:47 +0530

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element = {<Layout />}>
           <Route index element = {<Dashboard />} />
-          <Route path= "/" element = {<Products />} />
+          <Route path= "Products" element = {<Products />} />
 
       </Route>
     </Routes>
