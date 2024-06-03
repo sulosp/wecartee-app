da88418854fc19ae6c8afaa0789282cff99d7b39
sulosp <59638018+sulosp@users.noreply.github.com>
2024-06-03 14:26:15 +0530

    upcomingPromotions

--- client/src/components/buyer/upcomingPromotion.jsx
+++ client/src/components/buyer/upcomingPromotion.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { BUSINESS_DIRECTORY_LIST } from "../../lib/businessDirectoryList";
 import { HiChevronLeft } from "react-icons/hi";
 import { HiChevronRight } from "react-icons/hi";
@@ -6,24 +6,28 @@ import { HiChevronRight } from "react-icons/hi";
 export default function ImageCarousel() {
   const [currentPromotionIndex, setCurrentPromotionIndex] = useState(0);
 
-  // Filter out promotions with upcomingPromotion not null
   const promotionsWithImages = BUSINESS_DIRECTORY_LIST.filter(
     (item) => item.upcomingPromotion !== null
   );
 
-  const nextPromotion = () => {
+  const nextPromotion = useCallback(() => {
     setCurrentPromotionIndex(
       (prevIndex) => (prevIndex + 1) % promotionsWithImages.length
     );
-  };
+  }, [promotionsWithImages.length]);
 
-  const prevPromotion = () => {
+  const prevPromotion = useCallback(() => {
     setCurrentPromotionIndex(
       (prevIndex) =>
         (prevIndex - 1 + promotionsWithImages.length) %
         promotionsWithImages.length
     );
-  };
+  }, [promotionsWithImages.length]);
+
+  useEffect(() => {
+    const interval = setInterval(nextPromotion, 5000);
+    return () => clearInterval(interval);
+  }, [nextPromotion]);
 
   return (
     <div className="flex flex-col justify-start items-start h-full bg-white gap-5 p-20 rounded-lg">
@@ -37,28 +41,39 @@ export default function ImageCarousel() {
         >
           <HiChevronLeft />
         </button>
-        <div className="w-full">
-        <div className="relative w-60 z-1">
-          <img
-            className="absolute top-0 left-0 w-full h-full"
-            src={promotionsWithImages[(currentPromotionIndex - 1 + promotionsWithImages.length) % promotionsWithImages.length].upcomingPromotion}
-            alt={`Promotion ${currentPromotionIndex - 1}`}
-          />
-        </div>
-        <div className="relative w-80 z-3">
-          <img
-            className="w-full"
-            src={promotionsWithImages[currentPromotionIndex].upcomingPromotion}
-            alt={`Promotion ${currentPromotionIndex}`}
-          />
-        </div>
-        <div className="relative w-60 z-1">
-          <img
-            className="absolute top-0 left-0 w-full h-full"
-            src={promotionsWithImages[(currentPromotionIndex + 1) % promotionsWithImages.length].upcomingPromotion}
-            alt={`Promotion ${currentPromotionIndex + 1}`}
-          />
-        </div>
+        <div className="w-full inline-flex justify-center items-center">
+          <div className="relative w-96 h-64 z-2 overflow-hidden rounded-md">
+            <img
+              className="absolute top-0 left-0 w-full h-full transition duration-1000"
+              src={
+                promotionsWithImages[
+                  (currentPromotionIndex - 1 + promotionsWithImages.length) %
+                    promotionsWithImages.length
+                ].upcomingPromotion
+              }
+              alt={`Promotion ${currentPromotionIndex - 1}`}
+            />
+          </div>
+          <div className="relative w-56 h-48 -ml-100 z-1 overflow-hidden rounded-md">
+            <img
+              className="w-full h-full transition duration-1000"
+              src={
+                promotionsWithImages[currentPromotionIndex].upcomingPromotion
+              }
+              alt={`Promotion ${currentPromotionIndex}`}
+            />
+          </div>
+          <div className="relative w-36 h-24 -ml-100 z-0 overflow-hidden rounded-md">
+            <img
+              className="absolute top-0 left-0 w-full h-full transition duration-1000"
+              src={
+                promotionsWithImages[
+                  (currentPromotionIndex + 1) % promotionsWithImages.length
+                ].upcomingPromotion
+              }
+              alt={`Promotion ${currentPromotionIndex + 1}`}
+            />
+          </div>
         </div>
         <button
           onClick={nextPromotion}
