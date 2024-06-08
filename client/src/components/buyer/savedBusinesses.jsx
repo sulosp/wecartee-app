b2025cfee9a20fa2aae64d28443f0f2d25705c98
Sulochana P. <sulochanasp@hotmail.com>
2024-06-08 16:11:36 +0530

    filter

--- client/src/components/buyer/savedBusinesses.jsx
+++ client/src/components/buyer/savedBusinesses.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import SaveWidget from './saveWidget'
 
-export default function SavedBusinesses (){
+export default function SavedBusinesses() {
     return (
-        <div>Saved Businesses</div>
+        <div className="flex gap-10 flex-col justify-start items-center max-h-screen  p-24 bg-surface-100 w-full ">
+
+            <div className='w-full'><SaveWidget /></div>
+
+
+            <div className='inline-flex w-full justify-between items-start '>
+                <div className='flex flex-col justify-start gap-0'>
+                    <span className="text-primary-400 text-headline-sm">Saved Businesses</span>
+                    <span className="text-primary-400 text-body-md font-light gap-1 ">20 Businesses</span>
+
+                </div>
+            </div>
+
+        </div>
     )
 }
