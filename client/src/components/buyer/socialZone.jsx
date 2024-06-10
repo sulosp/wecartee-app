a95a7573d704eb6e43cc187259006d00567e1081
Sulochana P. <sulochanasp@hotmail.com>
2024-06-10 13:58:57 +0530

    wecartee

--- client/src/components/buyer/socialZone.jsx
+++ client/src/components/buyer/socialZone.jsx
@@ -11,15 +11,13 @@ export default function SocialZone() {
   // Find the store that matches the current URL
   const currentStore = STORE_DATA.find((store) => `/socialZone${store.url}` === currentURL);
   const storeId = currentStore ? currentStore.id : null;
-  
-
 
   return (
     <div className="flex flex-row w-full h-full relative flex-wrap">
       <div className="flex absolute w-2/12 z-2 h-full">
         <SearchBar />
       </div>
-  
+
       <Routes>
         <Route
           path="/"
@@ -32,22 +30,23 @@ export default function SocialZone() {
                     <p>{currentStore.tagline}</p>
                     <Link to={`/socialZone${currentStore.url}`}>Go to store</Link>
                   </div>
-                :
-                ''}
+                  :
+                  null}
               </div>
             </div>
           }
         />
-  
-        {/* Render the Store component if the current URL matches a store URL */}
-        {currentStore && (
+
+       {/* Render the Store component if the current URL matches a store URL */}
+       {currentStore && (
           <Route
-            key={currentStore.id}
+            key={currentStore.id} 
             path={`${currentStore.url}`}
-            element={<Store storeId={storeId} />} 
+            element={<Store storeId={storeId} />}
           />
         )}
-      </Routes>
+</Routes>
+      
     </div>
   );
 }
--- client/src/lib/storeData.jsx
+++ client/src/lib/storeData.jsx
@@ -3,6 +3,39 @@ export const STORE_DATA = [
     id: 1,
     name: "Nike",
     tagline: "Welcome to Nike Store",
+    url: '',
+    category: [
+      {
+        id: 1,
+        name: 'all',
+        img: "/assets/store/Nike/storeView.png",
+        position:" x: 0, y: 2, z: -3 "   
+        
+      },
+      {
+        id: 2,
+        women: 'women',
+        img: "/assets/store/Nike/storeView.png",
+        position:" x: 0, y: 2, z: -3"
+      },
+      {
+        id: 3,
+        women: 'Men',
+        img: "/assets/store/Nike/storeView.png",
+        position:" x: 0, y: 2, z: -3"
+      },
+      {
+        id: 4,
+        kids: 'kids',
+        img: "/assets/store/Nike/storeView.png",
+        position:" x: 0, y: 2, z: -3"
+      },
+    ]
+  },
+  {
+    id: 2,
+    name: "Nike",
+    tagline: "Welcome to Nike Store",
     url: "/nike",
     category: [
       {
@@ -35,7 +68,7 @@ export const STORE_DATA = [
   },
 
   {
-    id: 2,
+    id: 3,
     name: "Macdonalds",
     tagline: "Welcome to Macdonlads",
     url: "/macdonalds",
