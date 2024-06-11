ce41d332d3e15f3578450871e572d0825ec7f2b2
sulosp <59638018+sulosp@users.noreply.github.com>
2024-06-11 09:42:26 +0530

    text animation.jsx

--- client/src/components/controls/textAnimation.jsx
+++ client/src/components/controls/textAnimation.jsx
@@ -1,23 +1,21 @@
-import React from "react";
-import {useEffect} from 'react-router-dom';    
+import React, { useEffect, useState } from "react";
 
 export default function TextAnimation() {
-    
-    const text = [favorite, best - selling, amazing, food];
+    const text = ["favorite", "best-selling", "amazing", "food"];
 
     const t = text[Math.floor(Math.random() * text.length)];
 
-    const [currentText, setCurrentText] = React.useState(0);
-    React.useEffect(() => {
+    const [currentText, setCurrentText] = useState(0);
+    useEffect(() => {
         const interval = setInterval(() => {
             setCurrentText((prev) => (prev + 1) % text.length);
         }, 2000);
         return () => clearInterval(interval);
-    }, [t]);
+    }, []);
 
     return (
         <div>
-            <h1 className="animate-text">{text[currentText]}</h1>
+            <h1 className="animate-text">{t}</h1>
         </div>
     );
 }
