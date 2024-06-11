0c95eefcafe925550254cf90fb68082959db7fb0
Sulochana P. <sulochanasp@hotmail.com>
2024-06-11 13:21:43 +0530

    store.jsx

--- client/src/components/buyer/store.jsx
+++ client/src/components/buyer/store.jsx
@@ -3,7 +3,8 @@ import VR from "../buyer/vr";
 import { STORE_DATA } from "../../lib/storeData";
 import { Link, useLocation } from "react-router-dom";
 
-export default function Store(store) {
+
+export default function Store() {
   const location = useLocation();
   const currentURL = location.pathname;
 
@@ -17,17 +18,26 @@ export default function Store(store) {
             display: `/socialZone${store.url}` === currentURL ? "flex" : "none",
           }}
         >
-          <div className="flex flex-col p-50 justify-center items-center">
-            <div className="absolute z-1 top-0 w-full inline-flex justify-between items-center">
-              <div className="view-options btn-group p-5 rounded-full bg-white">
-                <Link to={`/socialZone${store.url}`}>{store.name}</Link>
-              </div>
-            </div>
 
-            <h1 className="z-1 text-white w-5/12 text-center text-display-lg">
-              {store.tagline}
-            </h1>
+          <div className="absolute z-1 top-0 w-full inline-flex justify-between items-center p-30">
+            <div className="view-options btn-group p-5 rounded-full bg-white">
+
+
+              {store.category.map((view) => (
+
+                <Link key={view.id} to={`/socialZone${store.url}/${view.id}`} >{view.name}</Link>
+
+              ))}
+
+
+
+            </div>
           </div>
+
+          <h1 className="z-1 text-white w-5/12 text-center text-display-lg">
+            {store.tagline}
+          </h1>
+
           <div className="absolute z-0 top-0 left-0 w-full h-full">
             {`/socialZone${store.url}` === currentURL && (
               <VR key={store.id} store={store} />
