a259a70e94329d04a9f6b63262cfcf50ebdf4dc8
Sulochana P. <59638018+sulosp@users.noreply.github.com>
2024-06-11 18:04:53 +0000

    saved Businesses table

--- client/src/components/buyer/savedBusinesses.jsx
+++ client/src/components/buyer/savedBusinesses.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-import BUSINESS_DIRECTORY_LIST from "../lib/businessDirectoryList";
+import React, { useState } from "react";
+import { BUSINESS_DIRECTORY_LIST } from "../../lib/businessDirectoryList";
+import Search from "../controls/search";
+import Filter from "../controls/filter";
 
 export default function SavedBusinesses() {
+    const [searchResults, setSearchResults] = useState([]);
     return (
         <div className="flex gap-10 flex-col justify-start items-center max-h-screen  p-24 bg-surface-100 w-full ">
             <div className="w-full p-20 rounded-lg flex flex-col justify-start items-start bg-white">
@@ -11,10 +14,27 @@ export default function SavedBusinesses() {
                             Saved Businesses
                         </span>
                         <span className="text-primary-400 text-body-md font-light gap-1 ">
-                            {BUSINESS_DIRECTORY_LIST.length} Businesses
+                            {BUSINESS_DIRECTORY_LIST.length + " Businesses"}
                         </span>
                     </div>
+                    <div className="inline-flex justify-end items-center gap-2">
+                        <Search setSearchResults={setSearchResults} />
+                        <Filter />
+                    </div>
                 </div>
+
+                <table className="w-full mt-5">
+                    <thead>
+                        <tr className="w-full inline-flex justify-between items-center pb-14 border-surface-100 border-b">
+                            <th>Business Name</th>
+                            <th>Category</th>
+                            <th>Products</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="w-full inline-flex justify-between items-center "></tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     );
