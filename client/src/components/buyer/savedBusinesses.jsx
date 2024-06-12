71b7c800b7958232190df1e2512203a725c3d00a
Sulochana P. <59638018+sulosp@users.noreply.github.com>
2024-06-12 07:09:41 +0000

    savedBusinesses table #2

--- client/src/components/buyer/savedBusinesses.jsx
+++ client/src/components/buyer/savedBusinesses.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BUSINESS_DIRECTORY_LIST } from "../../lib/businessDirectoryList";
 import Search from "../controls/search";
 import Filter from "../controls/filter";
+import Rating from "../controls/rating";
 
 export default function SavedBusinesses() {
     const [searchResults, setSearchResults] = useState([]);
@@ -26,13 +27,79 @@ export default function SavedBusinesses() {
                 <table className="w-full mt-5">
                     <thead>
                         <tr className="w-full inline-flex justify-between items-center pb-14 border-surface-100 border-b">
-                            <th>Business Name</th>
-                            <th>Category</th>
-                            <th>Products</th>
+                            <th className="basis-3/12 text-body-md text-primary-400 font-medium inline-flex justify-start items-center">
+                                Business Name
+                            </th>
+                            <th className="basis-6/12 text-body-md text-primary-400 font-medium inline-flex justify-start items-center">
+                                Category
+                            </th>
+                            <th className="basis-3/12 text-body-md text-primary-400 font-medium inline-flex justify-center items-center text-center">
+                                Products
+                            </th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr className="w-full inline-flex justify-between items-center "></tr>
+                    <tbody className="flex flex-col w-full justify-start items-center gap-2 mt-5">
+                        {searchResults.length > 0
+                            ? searchResults.map((seller) => (
+                                  <tr
+                                      key={seller.id}
+                                      className="w-full inline-flex justify-between items-center"
+                                  >
+                                      <td className="inline-flex justify-start items-center text-body-md text-primary-400 gap-3 p-3 basis-3/12 ">
+                                          <img
+                                              src={seller.iconPath}
+                                              alt="profile img"
+                                              className="w-10 h-10 rounded-full"
+                                          />
+                                          <div className="flex flex-col justify-center items-start min-h-full">
+                                              <span className="text-left">
+                                                  {seller.business}
+                                              </span>
+                                              <Rating
+                                                  rating={Math.floor(
+                                                      seller.rating,
+                                                  )}
+                                              />
+                                          </div>
+                                      </td>
+                                      <td className="inline-flex justify-start items-center text-body-md text-primary-400 gap-3 p-3 basis-6/12">
+                                          {seller.category.map(
+                                              (category, categoryIndex) => (
+                                                  <span
+                                                      key={categoryIndex}
+                                                      className="text-label-md text-primary-400 py-5 px-12 mx-3 rounded-full bg-primary-100"
+                                                  >
+                                                      {category}
+                                                  </span>
+                                              ),
+                                          )}
+                                      </td>
+                                  </tr>
+                              ))
+                            : BUSINESS_DIRECTORY_LIST.map((seller) => (
+                                  <tr
+                                      key={seller.id}
+                                      className="w-full inline-flex justify-between items-center"
+                                  >
+                                      <td className="inline-flex justify-start items-center text-body-md text-primary-400 gap-3 p-3 basis-3/12 ">
+                                          <img
+                                              src={seller.iconPath}
+                                              alt="profile img"
+                                              className="w-10 h-10 rounded-full"
+                                          />
+                                          <div className="flex flex-col justify-center items-start min-h-full">
+                                              <span className="text-left">
+                                                  {seller.business}
+                                              </span>
+                                              <Rating
+                                                  rating={Math.floor(
+                                                      seller.rating,
+                                                  )}
+                                              />
+                                          </div>
+                                      </td>
+                                  </tr>
+                              ))}
                     </tbody>
                 </table>
             </div>
