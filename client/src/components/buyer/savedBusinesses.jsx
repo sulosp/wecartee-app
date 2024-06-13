5036b71dfa86ca600f1b6b8a17dde6e6e0551eb2
Sulochana P. <59638018+sulosp@users.noreply.github.com>
2024-06-13 07:51:28 +0000

    savedBusinesses#2

--- client/src/components/buyer/savedBusinesses.jsx
+++ client/src/components/buyer/savedBusinesses.jsx
@@ -8,8 +8,8 @@ export default function SavedBusinesses() {
     return (
         <div className="gap-5 flex flex-col justify-start items-start max-h-screen bg-surface-100 w-full">
             <div className=" flex flex-col justify-start items-start h-auto gap-5 p-24 ">
-                <div className="inline-flex gap-5 justify-between items-center w-full bg-white shadow-lg p-20 rounded-lg sm:flex-wrap  ">
-                    <div className="flex flex-col text-primary-900">
+                <div className="inline-flex gap-5 justify-between items-center w-full bg-white shadow-lg p-20 rounded-lg md:flex-nowrap sm:flex-wrap  ">
+                    <div className="lg:basis-2/3  md:basis-full flex flex-col text-primary-900">
                         <span className="text-primary-700 text-headline-sm">
                             Saved Businesses
                         </span>
@@ -17,7 +17,7 @@ export default function SavedBusinesses() {
                             {BUSINESS_DIRECTORY_LIST.length + " "} Businesses
                         </span>
                     </div>
-                    <div className="inline-flex justify-center items-center gap-2 sm:w-full sm:justify-between">
+                    <div className="lg:basis-1/3 md:basis-full inline-flex justify-center items-center gap-2 sm:w-full sm:justify-between">
                         <Search />
                         <Filter />
                     </div>
