2c1f6c8d8a857bf235b8af0c0901d75f63424754
Sulochana P. <59638018+sulosp@users.noreply.github.com>
2024-06-13 07:53:15 +0000

    dashboard

--- client/src/components/buyer/dashboard.jsx
+++ client/src/components/buyer/dashboard.jsx
@@ -6,7 +6,7 @@ import UpcomingPromotion from "./upcomingPromotion.jsx";
 export default function Dashboard() {
   return (
     <div className="flex gap-5 flex-col justify-start items-center max-h-screen  bg-surface-100 w-full ">
-      <div className="flex flex-col h-auto w-auto p-24">
+      <div className="flex flex-col h-auto w-auto p-24 w-full">
         <div className="w-full flex p-20 bg-white rounded-lg justify-center items-center h-auto ">
           <OngoingPromotions />
         </div>
