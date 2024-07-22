0a183ac83adb739481ff1edfd7bb21062aa75cc1
Sulochana P. <59638018+sulosp@users.noreply.github.com>
2024-07-22 10:01:54 +0530

    cart.jsx

--- client/src/components/buyer/cart.jsx
+++ client/src/components/buyer/cart.jsx
@@ -4,19 +4,19 @@ import Filter from "../controls/filter";
 import ToggleButton from "../controls/toggleButton";
 import { HiOutlineHeart, HiOutlineShoppingBag } from "react-icons/hi";
 import { PRODUCTS_LIST } from "../../lib/productsList";
-import WishlistCard from "./wishlistCard";
-import CartCard from ".'-p;0/cartCard";
+import CartCard from "./cartCard";
 
 export default function Cart() {
   const [cartList, setCartList] = useState([]);
-  const [searchResults, setSearchResults] = useState([]);
 
   const handleCartList = (cartList) => {
     setCartList(cartList);
   }
-  
-  
 
+  const setSearchResults = (searchResults) => {
+    setCartList(searchResults);
+  }
+  
   return (
     <div className="gap-5 flex flex-col justify-start items-start max-h-screen bg-surface-100 w-full">
       <div className="flex flex-col w-full justify-start items-start h-auto gap-5 p-24">
@@ -32,29 +32,29 @@ export default function Cart() {
             iconSecondary={<HiOutlineShoppingBag />}
             labelPrimary="Wishlist"
             labelSecondary="Cart"
-            onPrimaryClick={handleCartList(cartList)}
-            onSecondaryClick={handleCartList(cartList)}
+            onPrimaryClick={() => handleCartList(cartList)}
+            onSecondaryClick={() => handleCartList(cartList)}
           />
         </div>
 
         <div className="flex flex-col w-full gap-2 justify-start items-center">
           {PRODUCTS_LIST.map((store) => (
-                <WishlistCard
-                  key={store.id}
-                  store={store}
-                  className="flex flex-col justify-stretch items-start w-full gap-2"
-                />
-              ))}
-            : 
-             { cartList.map((store) => (
-                <div key={store.id}>
-                  <CartCard
-                    store={store}
-                    className="flex flex-col justify-stretch items-start w-full gap-2"
-                  />
-                </div>
-              ))}
+            <div key={store.id}>
+              <CartCard
+                store={store}
+                className="flex flex-col justify-stretch items-start w-full gap-2"
+              />
+            </div>
+          ))}
         </div>
+        {cartList.map((store) => (
+          <div key={store.id}>
+            <CartCard
+              store={store}
+              className="flex flex-col justify-stretch items-start w-full gap-2"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
